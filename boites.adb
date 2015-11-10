714cf594d25c332ad97bd8099b86d4dcc7505e95
Réal <rmckay@hotmail.fr>
2015-11-10 12:00:03 +0100

    Ajout des paramètres et appel vers fonctions de dessins (à créer

--- boites.adb
+++ boites.adb
@@ -1,7 +1,7 @@
 
-function boites is
+function boites(t : String; epaisseur : Integer; l : String; longueur : Integer; w : String; largeur : Integer; q : String; longueur_queue : Integer; h : String; hauteur_externe : Integer; b : String; hauteur_interne : Integer; f, nom_fichier : String) is
 	
 begin
-	
-	
+	dessine_fond(epaisseur, longueur_queue, longueur, largeur);
+	dessine_cote(epaisseur, longueur_queue, longueur, largeur);
 end

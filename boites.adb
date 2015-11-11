bae76a36277041a3fa5e223f88970a25eb6ded0f
Réal <rmckay@hotmail.fr>
2015-11-11 16:48:59 +0100

    Définition et initialisation des variables de boites

--- boites.adb
+++ boites.adb
@@ -5,3 +5,27 @@ begin
 	dessine_fond(epaisseur, longueur_queue, longueur, largeur);
 	dessine_cote(epaisseur, longueur_queue, longueur, largeur);
 end
+
+procedure dessine_cote(t, q, l, w) is
+	longueur_usinee : Integer;
+	nb_queues_encoches : Integer;
+	place_restante : Integer;
+	distance_debut_encoches : Float;
+begin
+	--On enlève l'épaisseur en début et fin de coté'
+	longueur_usinee := l-2*t;
+	--On défini le nombre d'enchoches/queues à usiner'
+	nb_queues_encoches := Float'Floor (longueur_usinee/q);
+	
+	--On garde un nombre impair de queues/encoches
+	if nb_queues_encoches mod 2 = 0 then
+		nb_queues_encoches = nb_queues_encoches - 1;
+	end if;
+	
+	--Définition de la place restant après usinage (utile pour centrer les queues/encoches)
+	place_restante := l-2*t-n*q;
+	distance_debut_encoches := place_restante/2 + t;
+	
+	
+	
+end

7f929891a0de042528bee8d5a4aca8f42c37d3c6
Twixx <faucheux.seb@gmail.com>
2015-11-12 16:45:43 +0100

    Implementation dans le adb

--- svg.adb
+++ svg.adb
@@ -1,33 +1,41 @@
-
-
 package body svg is
+	fichier : File_type ;
+	procedure open_file(nomFichier : StrB.Bounded_String) is
+	begin
+		create(fichier, Out_File, To_String(nomFichier)) ;
+	end;
+	procedure close_file is
+	begin
+		close(fichier);
+	end;
 
-procedure open(width : Integer, height : Integer) is
-begin
-	put("<svg width=""");
-	put(height);
-	put(""" height =""");
-	put(width);
-	put(""">");
-end
+	procedure start(width, height : Natural) is
+	begin
+		Put(fichier,"<svg width=""");
+		Put(fichier,height);
+		Put(fichier,""" height =""");
+		Put(fichier,width);
+		Put(fichier,""">");
+	end;
 
-procedure close() is
-begin
-	put("</svg>")
-end
+	procedure stop is
+	begin
+		Put(fichier,"</svg>");
+	end;
 
-procedure lineFromTo(X1, Y1, X2, Y2) is
-begin
-<line x1="0" y1="0" x2="200" y2="200" style="stroke:rgb(255,0,0);stroke-width:2" />
-	put("<line x1=""");
-	put(X1);
-	put(""" y1="" ");
-	put(Y1);
-	put(""" x2="" ");
-	put(X2);
-	put(""" y2="" ");
-	put(Y2);
-	put(""" style=""stroke:rgb(255,0,0);stroke-width:2""/>");
-end;
+	procedure begin_polygon(x, y : Natural) is
+	begin
+	NULL;
+	end;
+
+	procedure end_polygon is
+	begin
+	NULL;
+	end;
+
+	procedure add_point(x, y : Natural) is
+	begin
+	NULL;
+	end;
 
 end svg;
